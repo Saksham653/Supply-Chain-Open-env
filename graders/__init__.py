182534f2b4b5c313a1af4e50a1f4534efc73d041
saksham653 <sakshamsrivastava7000@gamail.com>
2026-04-10 22:54:31 +0530

    fix import error

--- graders/__init__.py
+++ graders/__init__.py
@@ -1,5 +1,5 @@
-from .easy import grade as easy_grade
-from .medium import grade as medium_grade
-from .hard import grade as hard_grade
+from .easy import easy_grade
+from .medium import medium_grade
+from .hard import hard_grade
 
 __all__ = ["easy_grade", "medium_grade", "hard_grade"]
